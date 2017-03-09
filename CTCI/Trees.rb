7a0f1690baa4d3902bf8ff849b267c9df810b06e
Raymond Lee <raylee0525@gmail.com>
2017-03-08 23:56:55 -0800

    Minimal Tree

--- CTCI/Trees.rb
+++ CTCI/Trees.rb
@@ -1,4 +1,6 @@
 def route_nodes(start_node, end_node)
+  return true if start_noed == end_node
+
   queue = [start_node]
   visited = []
 
@@ -17,3 +19,26 @@ def route_nodes(start_node, end_node)
 
   false
 end
+
+
+class Node
+  attr_accessor :left, :right, :node
+
+  def initialize(node)
+    @node = node
+    @left = left
+    @right = right
+  end
+end
+
+def minimal_tree(array)
+  return array if array.length < 2
+
+  mid = array.length / 2
+  tree = Node.new(array[mid])
+  tree.left = minimal_tree(array[0...mid])
+  tree.right = minimal_tree(array[mid + 1..-1])
+  tree
+end
+
+p minimal_tree([1, 2, 3, 4, 5, 6, 7])
