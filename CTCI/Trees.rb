01e43b18282db500890422a11a74819ef758fb6d
Raymond Lee <raylee0525@gmail.com>
2017-03-08 23:30:30 -0800

    route between nodes

--- CTCI/Trees.rb
+++ CTCI/Trees.rb
@@ -1,3 +1,19 @@
-def route_nodes(graph, start, end)
+def route_nodes(start_node, end_node)
+  queue = [start_node]
+  visited = []
 
+  until queue.empty?
+    current = queue.shift
+    current.next_nodes.each do |node|
+      unless visited.include?(node)
+        if node == end_node
+          return true
+        else
+          queue << node
+        end
+      end
+    end
+  end
+
+  false
 end
