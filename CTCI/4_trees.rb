a6e6d5dabd380f2d9b416e28139f305affcc3b34
Raymond Lee <raylee0525@gmail.com>
2017-03-10 15:20:43 -0800

    Implemented Graph class

--- CTCI/4_trees.rb
+++ CTCI/4_trees.rb
@@ -1,5 +1,38 @@
+#Graph
+class Vertex
+  attr_reader :value, :in_edges, :out_edges
+
+  def initialize(value)
+    @value, @in_edges, @out_edges = value, [], []
+  end
+end
+
+class Edge
+  attr_reader :from_vertex, :to_vertex, :weight
+
+  def initialize(from_vertex, to_vertex, weight = 1)
+    self.from_vertex = from_vertex
+    self.to_vertex = to_vertex
+    self.weight = weight
+
+    to_vertex.in_edges << self
+    from_vertex.out_edges << self
+  end
+
+  def destroy!
+    self.to_vertex.in_edges.delete(self)
+    self.to_vertex = nil
+    self.from_vertex.out_edges.delete(self)
+    self.from_vertex = nil
+  end
+
+  protected
+
+  attr_writer :from_vertex, :to_vertex, :weight
+end
+
 def route_nodes(start_node, end_node)
-  return true if start_noed == end_node
+  return true if start_node == end_node
 
   queue = [start_node]
   visited = []
@@ -115,5 +148,5 @@ def valid_bst?(node, min, max)
 end
 
 def successor(node)
-  
+
 end
---
+++ Practice/practice.rb
@@ -0,0 +1,32 @@
+#Graph
+class Vertex
+  attr_reader :value, :in_edges, :out_edges
+
+  def initialize(value)
+    @value, @in_edges, @out_edges = value, [], []
+  end
+end
+
+class Edge
+  attr_reader :from_vertex, :to_vertex, :weight
+
+  def initialize(from_vertex, to_vertex, weight = 1)
+    self.from_vertex = from_vertex
+    self.to_vertex = to_vertex
+    self.weight = weight
+
+    to_vertex.in_edges << self
+    from_vertex.out_edges << self
+  end
+
+  def destroy!
+    self.to_vertex.in_edges.delete(self)
+    self.to_vertex = nil
+    self.from_vertex.out_edges.delete(self)
+    self.from_vertex = nil
+  end
+
+  protected
+  
+  attr_writer :from_vertex, :to_vertex, :weight
+end
