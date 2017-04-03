d25ad1d9c26ec94cb21c1e52111334095dc41918
Raymond Lee <raylee0525@gmail.com>
2017-04-03 16:13:34 -0700

    Implement python version of merge sort and merge

--- CTCI/Python/sorting.py
+++ CTCI/Python/sorting.py
@@ -16,7 +16,30 @@ def merge_sort(array):
     if len(array) < 2:
         return array
     mid = len(array) / 2
-    left = merge_sort(array[0:mid)
+    left = merge_sort(array[0:mid])
     right = merge_sort(array[mid:])
 
     return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while len(left) > j and len(right) > i:
+        if left[j] > right[i]:
+            result.append(right[i])
+            i += 1
+        else:
+            result.append(left[j])
+            j += 1
+
+    while j < len(left):
+        result.append(left[j])
+        j += 1
+    while i < len(right):
+        result.append(right[i])
+        i += 1
+
+    return result
+
+print merge_sort([1,2,45,1,3,4,7,2,1,100,312])
