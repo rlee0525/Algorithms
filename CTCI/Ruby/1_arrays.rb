e9b357a9f356f31505e8e6aa3e86d29683bac205
Raymond Lee <raylee0525@gmail.com>
2017-04-10 01:07:03 -0700

    Implement graph class - vertex and edge

--- CTCI/Ruby/1_arrays.rb
+++ CTCI/Ruby/1_arrays.rb
@@ -79,41 +79,6 @@ end
 p is_unique3("hello") == false
 p is_unique3("hiewqklm") == true
 
-class QuickSort
-  attr_reader :string
-
-  def initialize(string)
-    @string = string
-  end
-
-  def sort(left_index = 0, right_index = string.length - 1)
-    if left_index < right_index
-      pivot_index = rand(left_index..right_index)
-      new_pivot_index = partition(left_index, right_index, pivot_index)
-      sort(left_index, new_pivot_index - 1)
-      sort(new_pivot_index, right_index)
-    end
-
-    string
-  end
-
-  private
-
-  def partition(left_index, right_index, pivot_index)
-    pivot_value = string[pivot_index]
-    string[pivot_index], string[right_index] = string[right_index], string[pivot_index]
-    new_pivot_index = left_index
-    (new_pivot_index...right_index).each do |i|
-      if string[i] <= pivot_value
-        string[new_pivot_index], string[i] = string[i], string[new_pivot_index]
-        new_pivot_index += 1
-      end
-    end
-
-    new_pivot_index
-  end
-end
-
 # ------------------------------------------------------------------------------
 
 # Time O(N log N + M log M)
