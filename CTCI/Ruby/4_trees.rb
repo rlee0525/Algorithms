a57cb46340d598e84e6e4b1d1798f7ac12491e83
Raymond Lee <raylee0525@gmail.com>
2017-04-19 10:20:25 -0700

    implement route nodes - ruby

--- CTCI/Ruby/4_trees.rb
+++ CTCI/Ruby/4_trees.rb
@@ -9,7 +9,7 @@ class Vertex
   end
 end
 
-class Edges
+class Edge
   attr_reader :to_vertex, :from_vertex, :weight
 
   def initialize(to_vertex, from_vertex, weight = 1)
@@ -32,3 +32,45 @@ class Edges
 
   attr_writer :to_vertex, :from_vertex, :weight
 end
+
+# Test Graph
+a = Vertex.new(1)
+b = Vertex.new(2)
+c = Vertex.new(3)
+d = Vertex.new(4)
+e = Vertex.new(5)
+f = Vertex.new(6)
+Edge.new(a, b)
+Edge.new(c, d)
+Edge.new(e, f)
+Edge.new(a, d)
+Edge.new(b, e)
+Edge.new(a, c)
+
+# Time
+# Space
+def route_nodes(start_vertex, end_vertex)
+  return true if start_vertex == end_vertex
+
+  queue = [start_vertex]
+  visited = {}
+
+  until queue.empty?
+    current = queue.shift
+
+    current.out_edges.each do |edge|
+      return true if edge.to_vertex == end_vertex
+
+      unless visited[edge.to_vertex]
+        queue << edge.to_vertex
+      end
+    end
+
+    visited[current] = true
+  end
+
+  false
+end
+
+p route_nodes(a, c) == false
+p route_nodes(c, a) == true
