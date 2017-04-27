72d78a43a7b9e6fa64ba816b11fff030d8f5bd22
Raymond Lee <raylee0525@gmail.com>
2017-04-26 18:00:16 -0700

    Implement steal houses - robber in ruby

--- Others/Ruby/others.rb
+++ Others/Ruby/others.rb
@@ -2,3 +2,25 @@
 # You can not steal any adjacent houses. Given a list of non-negative
 # integers representing the amount of money of each house, determine the
 # maximum amount of money you can steal.
+
+def steal_houses(array)
+  return 0 if array.empty?
+
+  amounts = [0, array[0]]
+
+  i = 2
+  while i <= array.length
+    amounts[i] = [amounts[i - 1], amounts[i - 2] + array[i - 1]].max
+    i += 1
+  end
+
+  amounts[-1]
+end
+
+houses1 = [10, 1000000, 50, 50, 101]
+houses2 = [10, 10, 10, 10, 10]
+houses3 = [10, 100, 120, 110, 110, 30, 500, 10, 2, 20]
+
+p steal_houses(houses1) == 1000101
+p steal_houses(houses2) == 30
+p steal_houses(houses3) == 760
