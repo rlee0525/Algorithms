840281cb35e9429fb507fe90b60c997f2a25f448
Raymond Lee <raylee0525@gmail.com>
2017-05-02 11:39:12 -0700

    Initialize

---
+++ Others/JavaScript/CheckBox/checkbox.js
@@ -0,0 +1,15 @@
+document.getElementById("check-all").addEventListener("click", () => checkAll());
+
+const checkAll = () => {
+  let checkOne = document.getElementById("check-one");
+  let checkTwo = document.getElementById("check-two");
+  let checkThree = document.getElementById("check-three");
+
+  if (checkOne.checked === true) {
+    console.log("hi");
+  } else {
+    console.log("no");
+  }
+}
+
+const 
