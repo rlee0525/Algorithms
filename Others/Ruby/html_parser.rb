4e1d8a1b475b7f2332540ab1434aee79e3c96731
Raymond Lee <raylee0525@gmail.com>
2017-05-02 11:51:49 -0700

    Implement html_parser using Ruby

--- Others/Ruby/html_parser.rb
+++ Others/Ruby/html_parser.rb
@@ -1,4 +1,16 @@
 string = "<html><head></head><div><div></div></div></html>"
+# 0 => html, parent = html. count = 0
+# 1 => head, parent = html. count = 0 => 1
+# 2 => /head, parent = html. count = 1 => 0
+# 3 => div, parent = html. count = 0 => 1
+# 4 => div, parent = div. count = 1 => 2
+#
+#
+# html
+#   head
+#   /head
+#   div
+#     div
 
 class Tree
   attr_accessor :value, :children
@@ -11,15 +23,24 @@ end
 
 def html_parser(string)
   splitted = string.split("<")
-  root = Tree.new("<" + splitted[0])
+  root = Tree.new("<" + splitted[1])
   parent_node = root
+  count = 0
 
-  i = 1
+  i = 2
   while i < splitted.length
-    if splitted[i][0] != "/"
-      parent_node.children << ("<" + splitted[i])
-    else
-
+    if splitted[i][0] != "/" && count == 0
+      new_node = Tree.new("<" + splitted[i])
+      parent_node.children << new_node
+      count += 1
+    elsif splitted[i][0] != "/" && count > 0
+      new_node = Tree.new("<" + splitted[i])
+      parent_node.children << new_node
+      parent_node = parent_node.children[-1] if parent_node
+      count += 1
+    elsif splitted[i][0] == "/"
+      parent_node = parent_node.children[-1] if parent_node
+      count -= 1
     end
     i += 1
   end
