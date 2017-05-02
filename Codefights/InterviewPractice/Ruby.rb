4be271bb28353042b4f33ed99a3f7b0a7a787f2f
Raymond Lee <raylee0525@gmail.com>
2017-05-02 12:47:33 -0700

    Initialize codefights ruby

---
+++ Codefights/InterviewPractice/Ruby.rb
@@ -0,0 +1,9 @@
+def wordLadder(beginWord, endWord, wordList)
+
+end
+
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+p wordLadder(beginWord, endWord, wordList) == 5
