bd976fae2875b3c0846cf5d2f2be9f8f49054f6f
Raymond Lee <raylee0525@gmail.com>
2017-05-03 21:47:40 -0700

    Improve wordLadder slightly

--- Codefights/InterviewPractice/Ruby.rb
+++ Codefights/InterviewPractice/Ruby.rb
@@ -1,53 +1,5 @@
-# Version one - Find a path.
-def wordLadder(begin_word, end_word, word_list)
-  transform_count = 0
-  current_word = begin_word
-  queue = word_list
-  visited_count = 0
-
-  until queue.empty? || visited_count == queue.length
-    queue.delete(current_word) if queue.include?(current_word)
-    next_word = queue.shift
-    if oneLetter(current_word, next_word)
-      transform_count += 1
-      current_word = next_word
-      visited_count = 0
-    else
-      queue << next_word
-      visited_count += 1
-    end
-
-    break if current_word == end_word
-  end
-
-  if current_word == end_word
-    transform_count
-  else
-    0
-  end
-end
-
-def oneLetter(word1, word2)
-  difference = 0
-
-  (0...word1.length).each do |idx|
-    difference += 1 if word1[idx] != word2[idx]
-    return false if difference > 1
-  end
-
-  true
-end
-
-begin_word = "hit"
-end_word = "cog"
-word_list = ["hot", "dot", "dog", "lot", "log", "cog"]
-
-# shortest: "hit" -> "hot" -> "dot" -> "dog" -> "cog"
-
-p wordLadder(begin_word, end_word, word_list) == 5
-
-# Version 2 - Find the shortest version
-def wordLadder2(beginWord, endWord, wordList)
+# Find the shortest version
+def wordLadder(beginWord, endWord, wordList)
   visited = {}
   queue = [beginWord]
   count = 1
@@ -61,7 +13,7 @@ def wordLadder2(beginWord, endWord, wordList)
     end
 
     wordList.each do |word|
-      if oneLetter(current_word, word) && !visited[word]
+      if !visited[word] && oneLetter(current_word, word)
         queue << [word, count + 1]
         visited[word] = true
 
@@ -72,6 +24,18 @@ def wordLadder2(beginWord, endWord, wordList)
 
   0
 end
+
+def oneLetter(word1, word2)
+  difference = 0
+
+  (0...word1.length).each do |idx|
+    difference += 1 if word1[idx] != word2[idx]
+    return false if difference > 1
+  end
+
+  true
+end
+
 #
 # ["hot", "mit", "dot", "dog", "lot", "log", "cog"]
 #
@@ -89,4 +53,4 @@ beginWord = "hit"
 endWord = "cog"
 wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
 
-p wordLadder2(beginWord, endWord, wordList) #== 5
+p wordLadder(beginWord, endWord, wordList) == 5
