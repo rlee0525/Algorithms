21ae2045b0fdff4fe20e96036e58b639907eecab
Raymond Lee <raylee0525@gmail.com>
2017-05-31 21:11:55 -0700

    cf - closestnum

--- Companies/Palantir/ruby.rb
+++ Companies/Palantir/ruby.rb
@@ -159,7 +159,58 @@ def isCryptSolution(crypt, solution)
   decrypt_first.to_i + decrypt_second.to_i == decrypt_third.to_i  
 end
 
+# https://codefights.com/interview/XhYvPHb96yJ3idYs5/companies/5xHhsMkmS5vrFDGLP
 
+# Given an array of integers nums and an integer k, determine whether there are two distinct indices i and j in the array where nums[i] = nums[j] and the absolute difference between i and j is less than or equal to k.
+
+# Example
+
+# For nums = [0, 1, 2, 3, 5, 2] and k = 3, the output should be
+# containsCloseNums(nums, k) = true.
+
+# There are two 2s in nums, and the absolute difference between their positions is exactly 3.
+
+# For nums = [0, 1, 2, 3, 5, 2] and k = 2, the output should be
+
+# containsCloseNums(nums, k) = false.
+
+# The absolute difference between the positions of the two 2s is 3, which is more than k.
+
+# Input/Output
+
+# [time limit] 4000ms (rb)
+# [input] array.integer nums
+
+# Guaranteed constraints:
+# 0 ≤ nums.length ≤ 55000,
+# -231 - 1 ≤ nums[i] ≤ 231 - 1.
+
+# [input] integer k
+
+# Guaranteed constraints:
+# 0 ≤ k ≤ 35000.
+
+# [output] boolean
+
+def containsCloseNums(nums, k)
+  hash = Hash.new { |h, k| h[k] = [] }
+
+  nums.each_with_index do |num, idx|
+    hash[num] << idx
+  end
+
+  hash.each do |key, v|
+    if v.length > 1
+      (0...v.length).each do |i|
+        (i + 1...v.length).each do |j|
+          return true if v[j] - v[i] <= k
+        end
+      end
+    end
+  end  
+
+  false
+end
 
 
 
