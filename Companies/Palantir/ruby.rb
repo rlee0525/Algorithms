2be542cf5dd40c996511eb4bc34e96e4394001c7
Raymond Lee <raylee0525@gmail.com>
2017-05-31 19:23:50 -0700

    cf - containduplicate

--- Companies/Palantir/ruby.rb
+++ Companies/Palantir/ruby.rb
@@ -29,9 +29,43 @@ def singleNumber(nums)
   bitwise
 end
 
+# https://codefights.com/interview/xQYfEWxBsnFP4Jmjh/companies/5xHhsMkmS5vrFDGLP
 
+# Given an array of integers, write a function that determines whether the array contains any duplicates. Your function should return true if any element appears at least twice in the array, and it should return false if every element is distinct.
 
+# Example
 
+# For a = [1, 2, 3, 1], the output should be
+# containsDuplicates(a) = true.
+
+# There are two 1s in the given array.
+
+# For a = [3, 1], the output should be
+# containsDuplicates(a) = false.
+
+# The given array contains no duplicates.
+
+# Input/Output
+
+# [time limit] 4000ms (rb)
+# [input] array.integer a
+
+# Guaranteed constraints:
+# 0 ≤ a.length ≤ 105,
+# -2 · 109 ≤ a[i] ≤ 2 · 109.
+
+# [output] boolean
+
+def containsDuplicates(a)
+  seen = {}
+  
+  a.each do |num|
+    return true if seen[num]
+    seen[num] = true
+  end
+  
+  false
+end
 
 
 
