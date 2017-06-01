492117474b8520314dcdcbb4a366aeb73773f822
Raymond Lee <raylee0525@gmail.com>
2017-05-31 19:22:07 -0700

    cf - singlenumber

--- Companies/Palantir/ruby.rb
+++ Companies/Palantir/ruby.rb
@@ -1,4 +1,47 @@
 # CodeFights
+# https://codefights.com/interview/7xqHvuKXT2oBJ26iq/companies/5xHhsMkmS5vrFDGLP
+
+# You are given an array of integers in which every element appears twice, except for one. Find the element that only appears one time. Your solution should have a linear runtime complexity (O(n)). Try to implement it without using extra memory.
+
+# Example
+
+# For nums = [2, 2, 1], the output should be
+# singleNumber(nums) = 1.
+
+# Input/Output
+
+# [time limit] 4000ms (rb)
+# [input] array.integer nums
+
+# Guaranteed constraints:
+# 1 ≤ nums.length ≤ 104,
+# -109 ≤ nums[i] ≤ 109.
+
+# [output] integer
+
+def singleNumber(nums)
+  bitwise = nums[0]
+
+  (1...nums.length).each do |idx|
+    bitwise ^= nums[idx]
+  end
+
+  bitwise
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# -----------------
+# CodeFights
 
 # sumSubsets
 # https://codefights.com/interview/kEgA4DXcfXuriqGru/companies/5xHhsMkmS5vrFDGLP
