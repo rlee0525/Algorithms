85a98cef288916b2b6c45946eb8f830985007193
Raymond Lee <raylee0525@gmail.com>
2017-06-12 14:00:04 -0700

    kth largest element

--- Companies/Apple/Ruby.rb
+++ Companies/Apple/Ruby.rb
@@ -221,3 +221,32 @@ def numberOf1Bits(n)
   count
 end
 
+# Note: Avoid using built-in std::nth_element (or analogous built-in functions in languages other than C++) when solving this challenge. Implement them yourself, since this is what you would be asked to do during a real interview.
+
+# Find the kth largest element in an unsorted array. This will be the kth largest element in sorted order, not the kth distinct element.
+
+# Example
+
+# For nums = [7, 6, 5, 4, 3, 2, 1] and k = 2, the output should be
+# kthLargestElement(nums, k) = 6;
+# For nums = [99, 99] and k = 1, the output should be
+# kthLargestElement(nums, k) = 99.
+# Input/Output
+
+# [time limit] 4000ms (rb)
+# [input] array.integer nums
+
+# Guaranteed constraints:
+# 1 ≤ nums.length ≤ 105,
+# -105 ≤ nums[i] ≤ 105.
+
+# [input] integer k
+
+# Guaranteed constraints:
+# 1 ≤ k ≤ nums.length.
+
+# [output] integer
+
+def kthLargestElement(nums, k)
+  nums.sort[-k]
+end
