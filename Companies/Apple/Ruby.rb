ecfaf819d294be2836698c116013f17c909f66f0
Raymond Lee <raylee0525@gmail.com>
2017-06-12 14:21:51 -0700

    higherversion2

--- Companies/Apple/Ruby.rb
+++ Companies/Apple/Ruby.rb
@@ -250,3 +250,54 @@ end
 def kthLargestElement(nums, k)
   nums.sort[-k]
 end
+
+# You have two version strings composed of several non-negative decimal fields that are separated by periods ("."). Both strings contain an equal number of numeric fields. Return 1 if the first version is higher than the second version, -1 if it is smaller, and 0 if the two versions are the same.
+
+# The syntax follows the regular semver (semantic versioning) ordering rules:
+
+# 1.0.5 < 1.1.0 < 1.1.5 < 1.1.10 < 1.2.0 < 1.2.2
+# < 1.2.10 < 1.10.2 < 2.0.0 < 10.0.0
+# Example
+
+# For ver1 = "1.2.2" and ver2 = "1.2.0", the output should be
+# higherVersion2(ver1, ver2) = 1;
+# For ver1 = "1.0.5" and ver2 = "1.1.0", the output should be
+# higherVersion2(ver1, ver2) = -1;
+# For ver1 = "1.0.5" and ver2 = "1.00.05", the output should be
+# higherVersion2(ver1, ver2) = 0.
+# Input/Output
+
+# [time limit] 4000ms (rb)
+# [input] string ver1
+
+# Correct version as a string.
+
+# Guaranteed constraints:
+
+# 1 ≤ ver1.length ≤ 350.
+
+# [input] string ver2
+
+# Correct version as a string.
+
+# Guaranteed constraints:
+
+# 1 ≤ ver2.length ≤ 350.
+
+# [output] integer
+
+# Return 1 if ver1 is higher than ver2, -1 if it's smaller, and 0 if the two versions are the same.
+
+def higherVersion2(ver1, ver2)
+  return 0 if ver1 == ver2
+
+  ver1 = ver1.split(".").map(&:to_i)
+  ver2 = ver2.split(".").map(&:to_i)
+
+  (0..2).each do |idx|
+    return 1 if ver1[idx] > ver2[idx]
+    return -1 if ver1[idx] < ver2[idx]
+  end
+
+  0
+end
