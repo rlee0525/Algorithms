a3265f3a1d9d2532c0b92209abb812b8e82a1362
Raymond Lee <raylee0525@gmail.com>
2017-06-12 20:27:41 -0700

    initialize maximalsquare

--- Companies/Apple/Ruby.rb
+++ Companies/Apple/Ruby.rb
@@ -426,8 +426,62 @@ end
 
 # recursion
 def climbingStairs(n)
+  return 0 if n <= 0
   return 1 if n == 1
   return 2 if n == 2
   
   climbingStairs(n - 1) + climbingStairs(n - 2)
+end
+
+# DP
+def climbingStairs(n)
+  return 0 if n <= 0
+  return 1 if n == 1
+  return 2 if n == 2
+
+  n_minus_one = 2
+  n_minus_two = 1
+  all = 0
+
+  (2...n).each do |i|
+    all = n_minus_one + n_minus_two
+    n_minus_two = n_minus_one
+    n_minus_one = all
+  end
+  
+  all
+end
+
+# You have a 2D binary matrix that's filled with 0s and 1s. In the matrix, find the largest square that contains only 1s and return its area.
+
+# Example
+
+# For
+
+# matrix = [
+#     ['1', '0', '1', '1', '1'],
+#     ['1', '0', '1', '1', '1'],
+#     ['1', '1', '1', '1', '1'],
+#     ['1', '0', '0', '1', '0'],
+#     ['1', '0', '0', '1', '0']
+# ]
+# the output should be
+# maximalSquare(matrix) = 9.
+
+# Input/Output
+
+# [time limit] 4000ms (rb)
+# [input] array.array.char matrix
+
+# Guaranteed constraints:
+# 0 ≤ matrix.length ≤ 100,
+# 1 ≤ matrix[i].length ≤ 100,
+# 0 ≤ matrix[i][j] ≤ 1.
+
+# [output] integer
+
+# An integer that represents the area of the largest square in the given matrix that is composed only of 1s.
+
+def maximalSquare(matrix)
+
 end
