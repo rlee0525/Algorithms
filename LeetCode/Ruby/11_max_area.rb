584cf8c91812fccf6f217f6ab06d9207df8c7495
Raymond Lee <raylee0525@gmail.com>
2017-06-16 11:12:28 -0700

    leetcode - 11 TLE

--- LeetCode/Ruby/11_max_area.rb
+++ LeetCode/Ruby/11_max_area.rb
@@ -3,3 +3,20 @@
 # ind two lines, which together with x-axis forms a container, such that the container contains the most water.
 
 # Note: You may not slant the container and n is at least 2.
+
+def max_area(height)
+  max_area = 0
+
+  (0...height.length).each do |idx|
+    (idx + 1...height.length).each do |idx2|
+      h = [height[idx], height[idx2]].min
+      area = h * (idx2 - idx)
+      max_area = area > max_area ? area : max_area
+    end
+  end
+  
+  max_area
+end
+
+# Time O(N^2) TLE
+
