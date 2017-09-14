edd740c44a231a7321a56f6979d616956afb8347
Raymond Lee <raylee0525@gmail.com>
2017-09-14 19:20:14 +0530

    update reddit

---
+++ Others/Reddit/reddit-challenge/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from './search_bar.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
