a52eca6bfccfe98e72b3bd85d36c80dda6a29bc7
Raymond Lee <raylee0525@gmail.com>
2017-11-07 13:56:31 -0800

    parse object

--- Algorithms.js
+++ Algorithms.js
@@ -20,6 +20,36 @@ const secondSmallest = arr => {
 
 console.log(secondSmallest([2, 10, 4, 35, -1, 20, 33, 100]) === 2);
 
+// 2) Parse Object
+const parseObj = obj => {
+  let res = [];
+  let key;
+
+  for (key in obj) {
+    if (typeof obj[key] === 'string') {
+      res.push(obj[key]);
+    } else if (typeof obj[key] === 'object') {
+      res.push(parseObj(obj[key]));
+    } else {
+      res.push("");
+    }
+  }
+
+  return res.join(", ");
+};
+
+let json2 = {
+  "Name": "Raymond Lee",
+  "Position": "Software Engineer",
+  "Details": {
+    "id": "25",
+    "Alma mater": "Northwestern University",
+    "Random Num": 300
+  }
+};
+
+console.log(parseObj(json2) === "Raymond Lee, Software Engineer, 25, Northwestern University, ");
+
 // Medium
 
 // Hard
