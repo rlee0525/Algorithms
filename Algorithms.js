1f69a6e2a152dd9b7e9e38a327514c7c2ef34f45
Raymond Lee <raylee0525@gmail.com>
2017-11-07 13:51:44 -0800

    second smallest

--- Algorithms.js
+++ Algorithms.js
@@ -1,5 +1,24 @@
 // Easy
+// 1) Second Smallest
+const secondSmallest = arr => {
+  let smallest = 1.0 / 0.0;
+  let second = 1.0 / 0.0;
 
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+
+    if (num <= smallest) {
+      second = smallest;
+      smallest = num;
+    } else if (num < second) {
+      second = num;
+    }
+  }
+
+  return second;
+};
+
+console.log(secondSmallest([2, 10, 4, 35, -1, 20, 33, 100]) === 2);
 
 // Medium
 
