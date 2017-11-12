977ff4313c1c0d6bc66fde5af737b2ceb87cfb00
Raymond Lee <raylee0525@gmail.com>
2017-11-12 08:16:38 -0800

    longest valid parens

--- Algorithms.js
+++ Algorithms.js
@@ -775,6 +775,22 @@ const isValidBST = (root, min = -1.0 / 0.0, max = 1.0 / 0.0) => {
   return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
 };
 
+// Longest valid parentheses
+const longestValidParentheses = string => {
+  let stack = [-1];
+  let longest = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ")" && stack.length !== 0 && string[stack[stack.length - 1]] === "(") {
+      stack.pop();
+      longest = Math.max(longest, i - stack[stack.length - 1]);
+    } else {
+      stack.push(i);
+    }
+  }
+
+  return longest;
+};
 
 
 // Find the middle element of a linked list
